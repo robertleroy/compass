777d70b49d60c8f6c942d2cb1d24a5cc84b97252
Rob Leroy <robleroy@msn.com>
2025-03-24 07:41:57 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  let doe = "Hello World";
+
+  onMount(() => {
+    if (window && window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+      // doe = 'Supported!';
+      console.log(window);
+    }
+  });
+</script>
+
+<h2 class="">
+  {doe}
+</h2>
+
+<style>
+
+</style>
